096fc57fab6f9b923d4365316e44a328c2e11cae
estrellaaagc <115511608+estrellaaagc@users.noreply.github.com>
2022-11-08 13:47:25 -0700

    Add files via upload

---
+++ desarrolla_aplicaciones_web-main/parcial2/PracticaWebDatos/eliminarRegistro.php
@@ -0,0 +1,22 @@
+<?php
+
+    include 'conexion.php';
+
+    $id = $_GET["id"];
+
+    $sql = "DELETE FROM usuarios WHERE id=" . $id;
+
+    if($conexion->query($sql) === TRUE){
+        echo "Se eliminó el registro con ID " .$id. "<br>";
+        echo "<a href='consultarDatos.php'>Regresar</a>";
+    } else {
+        echo "Ocurrió un error: " . $conexion->error ."<br>";
+        echo "<a href='consultarDatos.php'>Regresar</a>";
+    }
+
+?>
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
